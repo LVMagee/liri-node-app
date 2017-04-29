bc1a7d6577fb3885a02da18c1b3052c1ffdcf034
LV Magee <lvmagee@gmail.com>
2017-04-28 22:31:13 -0400

    added js files and json

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'DjIBuhqUQd9WVHHgQbcUdYZ3S',
+  consumer_secret: 'nrztdZ7awynF2EkCpf4omrZgkyT0tkr0CHG0GWoA6BCdmfxdhL',
+  access_token_key: '2281419655-GAMwhs9lVkSCKhzeTxRnK4anJd42JDuC9Fbw152',
+  access_token_secret: 'fuqrsNOnsiofpc13ppvEtneyiJQruOuP83xMVGBhAJsqq',
+}
